199e9522c81de9ffcea34bcc4ae1ed34ca7cc9d5
Long Van <longvan121996@gmail.com>
2025-01-11 22:35:12 +0900

    first commit

--- src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export interface FacebookRequest extends Request {
-    user?: any; // Add user type if you have a specific user model
-}
-
-export interface FacebookResponse extends Response {
-    // Add any custom response properties if needed
-}
--- src/types/user.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface IUser extends Document {
-    facebookId: string;
-    name: string;
-    // Thêm các thuộc tính khác nếu cần
-}
